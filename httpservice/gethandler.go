05863fb925a80cfac6f04f13f626f0bb3eccb692
henosteven <henosteven@126.com>
2018-11-21 16:30:00 +0800

    调整gethandler实现

--- httpservice/gethandler.go
+++ httpservice/gethandler.go
@@ -2,8 +2,31 @@ package httpservice
 
 import (
 	"net/http"
+	"jinjing.space/web/model"
+	"strconv"
+)
+
+const (
+	USERID_EMPTY = "user id empty"
+	USERID_NOT_DIGIT = "user id not digit"
 )
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("jinjing"))
+	params := r.URL.Query()
+	userID := params.Get("userId")
+	if userID == "" {
+		w.Write([]byte(USERID_EMPTY))
+	}
+
+	id, err := strconv.Atoi(userID)
+	if err !=nil {
+		w.Write([]byte(USERID_NOT_DIGIT))
+	}
+
+	userName, err := model.GetUserNameByID(id)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}  else {
+		w.Write([]byte(userName))
+	}
 }
